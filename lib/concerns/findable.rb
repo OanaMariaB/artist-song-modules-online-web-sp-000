f3b943b7c2aa5ffcf04333d9595cd1739d577ad6
Oana Berentan <oana.berentan@yahoo.com>
2019-06-23 18:00:49 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -2,11 +2,11 @@ module findable
   module ClassMethods
 
     def find_by_name(name)
-      @@artists.detect{|a| a.name == name}
+      self.detect{|a| a.name == name}
     end
 
     def find_by_name(name)
-      @@songs.detect{|a| a.name == name}
+      self.detect{|a| a.name == name}
     end
 
   end
